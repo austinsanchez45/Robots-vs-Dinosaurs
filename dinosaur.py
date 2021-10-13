156cdd833fbdafcf761f3a8dfcf3150cc5d08498
austin sanchez <austinsanchez45@yahoo.com>
2021-10-13 11:40:18 -0500

    I have created a starting point for all files except main.

---
+++ dinosaur.py
@@ -0,0 +1,9 @@
+from robot import Robot
+class Dinosaur:
+    def __init__(self, name, attack_power):
+        self.name = ""
+        self.attack_power = 0
+        self.health = 0
+
+    def attack(self, Robot):
+        pass
---
+++ robot.py
@@ -0,0 +1,9 @@
+from weapon import Weapon
+class Robot:
+    def __init__(self):
+        self.name = ""
+        self.health = 0
+        self.weapon = Weapon()
+
+    def attack(self, dinosaur):
+        pass
