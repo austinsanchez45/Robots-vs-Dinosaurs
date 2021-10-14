1fe05685d57b31d200feefa91e9d8acae27f8984
austin sanchez <austinsanchez45@yahoo.com>
2021-10-14 11:46:08 -0500

    Set up the attack method in robot to track the battle status between them and the dinosaurs, showing what weapons are used and remaining health of the dinosaurs. Also created a method for choosing what weapon a robot uses.

--- robot.py
+++ robot.py
@@ -7,4 +7,16 @@ class Robot:
         self.weapon = Weapon()
 
     def attack(self, dinosaur):
-        pass
+        print(f"\n{self.name} blasts the {dinosaur.name} with their {self.weapon}!")
+        dinosaur.health -= self.weapon.attack_power
+        print(f"\n{dinosaur.name} has {dinosaur.health} HP remaining!")
+        if dinosaur.health <= 0:
+            dinosaur.alive = False
+
+    def weapon_selection(self):
+        weapon_choice = input(f"""\nWhich weapon would you like to use for {self.name}?
+        \n1: Plasma Rifle: Attack Power - 20
+        \n2: Sword: Attack Power - 15
+        \n3: Railgun: Attack Power - 50
+        \n""")
+        self.weapon.weapons(weapon_choice)
